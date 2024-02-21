9373fb59eafd716f3daa86a9ddb2b1292445c973
Usuario <Usuario@DESKTOP-F560U2J>
2024-02-21 00:08:49 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto/Models/Empleado.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto.Models
+{
+    [Table("Empleados")]
+
+    public class Empleado
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido_p { get; set; }
+
+        public virtual Jefe Jefe { get; set; }
+
+        public string Area { get; set; }
+
+    }
+}
